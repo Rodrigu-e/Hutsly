fbdfd13f583321f0cbe94427c511d88ebbf1a5fb
Rodrigo Breda Pinheiro <BredaRodrigoPinheiro@gmail.com>
2024-02-22 22:45:27 +0100

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {},
+		colors: {
+			'bg-grey': '#F7F8F8',
+			'pure-white': '#ffffff',
+			star: '#FFC250',
+			'pure-black': '#000000',
+			'light-text': '#2A3347',
+			'bg-footer': '#111B31',
+			transarent: 'rgba(255, 255, 255, 0.03)',
+		},
+	},
+	plugins: [],
+};
